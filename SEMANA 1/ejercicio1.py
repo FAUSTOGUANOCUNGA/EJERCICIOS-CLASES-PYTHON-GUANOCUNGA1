46b50c948b8f3d21b9a102cd6b122b01c8f3fd89
FAUSTOGUANOCUNGA <fausto.guanocunga@ister.edu.ec>
2026-05-17 00:34:55 -0500

    primer commit

---
+++ SEMANA 1/ejercicio1.py
@@ -0,0 +1,8 @@
+import sys
+
+print("verificando tu instalación de Python...")
+print("-" * 45)
+print(f"Versión de Python: {sys.version.split()[0]}")
+print(f"Sistema Operativo: {sys.platform}")
+print("-" * 45)
+print("¡Tu entorno está listo para el ciclo 2026!")
---
+++ SEMANA 1/main.py
@@ -0,0 +1,14 @@
+nombre = 'Javier'
+apellido = "Guanocunga"
+materia = """PROGRAMACION PYTHON"""
+edad = 37
+listas = [1, 2, 3, 4, 5]
+cerrar = True
+abrir = False
+
+
+print(nombre, apellido)
+
+print(f"nombre: {nombre} {apellido}")
+print(materia)
+
