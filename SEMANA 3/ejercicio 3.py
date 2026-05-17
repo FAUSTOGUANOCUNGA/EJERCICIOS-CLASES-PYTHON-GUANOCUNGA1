9ac66b9dc07dd323f033dcf051bef9fd46a093ae
FAUSTOGUANOCUNGA <fausto.guanocunga@ister.edu.ec>
2026-05-17 18:51:36 -0500

    primer commit

--- SEMANA 3/ejercicio 3.py
+++ SEMANA 3/ejercicio 3.py
@@ -1,32 +1,31 @@
-def funcion1():...
+
+
+def funcion1():
     try :
         valor_1 = input("Introduce un numero: ")
         valor_2 = input("Introduce otro numero: ")
-        total = int(valor_1) + (valor_2)
+        total = int(valor_1) + int(valor_2)
     except Exception as e:
         total = 0
     return total
 
-
 total_general_1 = funcion1()
 total_general_2 = funcion1()
-total_general_3 = total_general_1 + total_general_2()
-print(f"Resultado total: {total_general_3})
+total_general_3 = total_general_1 + total_general_2
+print(f"Resultado total:{total_general_3}")
 
 
-import math
-def calcular_area_circulo(radio):
-    area = math.pi * (radio ** 2)
-    return area
-radio_usuario = 5
-resultado = calcular_area_circulo(radio_usuario)
-print(f"El área del circulo con radio: {radio_usuario} es")
 
+def calcular_edad():
+    try :
+        edad_1 = input("Introduce un numero: ")
+        edad_2 = input("Introduce otro numero: ")
+        total = int(edad_1) + int(edad_2)
+    except Exception as e:
+        total = 0
+    return total
 
-def calcular_edad(anio_nacimiento):
-    anio_actual = 2026
-    edad = anio_actual - anio_nacimiento
-    return f"Tienen {edad} años."
-
-mi_edad = calcular_edad(1988)
-print (mi_edad)
+total_general_11 = calcular_edad()
+total_general_22 = calcular_edad()
+total_general_33 = total_general_11 + total_general_22
+print(f"Resultado total:{total_general_33}")
