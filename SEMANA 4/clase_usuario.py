5d088c5b48beb9d533c9e185cffd60ce83082caf
FAUSTOGUANOCUNGA <fausto.guanocunga@ister.edu.ec>
2026-07-29 11:48:44 -0500

    primer commit

---
+++ SEMANA 4/clase_usuario.py
@@ -0,0 +1,10 @@
+
+
+Ejercicio 02
+
+class Usuario:
+    def __init__(self):
+        pass
+    def autenticar(self, intento: object) -> None:
+        pass
+autenticar = Usuario()
---
+++ SEMANA 4/clase_usuario_ejecución.py
@@ -0,0 +1,4 @@
+import clase_usuario
+
+usuario_variable = clase_usuario.Usuario
+usuario_variable.autenticar()
