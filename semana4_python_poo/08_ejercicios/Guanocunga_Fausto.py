5d1ad8a8cbcafdbb7ccdba7f3ad828ab4b5da3ae
FAUSTOGUANOCUNGA <fausto.guanocunga@ister.edu.ec>
2026-07-29 21:17:47 -0500

    primer commit

--- semana4_python_poo/08_ejercicios/Guanocunga_Fausto.py
+++ semana4_python_poo/08_ejercicios/Guanocunga_Fausto.py
@@ -9,23 +9,22 @@ class ImpresoraRed:
     def imprimir(self, paginas):
         # Validación opcional para el reto EXTRA: evitar números negativos
         try:
-
-        if paginas > 0:
-            self.paginas_impresoras += paginas
-        else:
-            raise NameError("Cantidad no valida")
-            except Exception as e:
+            if paginas > 0:
+                self.paginas_impresoras += paginas
+                if self.paginas_impresoras >= self.limite_impresion:
+                    print("limite de impresion alcanzado, por favor recargue papel")
+            else:
+                raise NameError("Cantidad no valida")
+        except Exception as e:
             print(e)
 
     def reportar(self):
         print(f"Modelo: {self.modelo}, IP: {self.ip}, Paginas: {self.paginas_impresoras}")
 
 
-
-# --- PRUEBAS ---
 imp = ImpresoraRed("10.0.0.50", "HP-LaserJet")
 imp.imprimir(3)
-imp.imprimir(5)
+imp.imprimir(1)
 imp.reportar()
 
 # Prueba del reto EXTRA:
