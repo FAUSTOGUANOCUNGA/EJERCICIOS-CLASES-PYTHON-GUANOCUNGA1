804438f8db7aa58b941fdbc18003a33dd8113680
FAUSTOGUANOCUNGA <fausto.guanocunga@ister.edu.ec>
2026-08-01 14:57:27 -0500

    primer commit

---
+++ semana4_python_poo/06_pilares_poo/pratica_pilares_poo.py
@@ -0,0 +1,11 @@
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent))
+
+from encapsulamiento import CuentaSinProteccion
+
+print("=== CuentaSinProteccion===")
+
+
+
+
