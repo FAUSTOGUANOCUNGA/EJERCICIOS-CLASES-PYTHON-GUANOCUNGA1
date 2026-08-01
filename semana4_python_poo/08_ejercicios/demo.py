57afc561250d16a5743b979a07064dd070a1fb95
FAUSTOGUANOCUNGA <fausto.guanocunga@ister.edu.ec>
2026-08-01 14:25:25 -0500

    primer commit

--- semana4_python_poo/08_ejercicios/demo.py
+++ semana4_python_poo/08_ejercicios/demo.py
@@ -12,5 +12,24 @@ class Dispositivo:
         print(f"IP: {self.ip}, MAC: {self.mac}, Color: {self.color}")
 
 #Crear el objeto a usarlo:
-d= Dispositivo("172.16.31.10", "AA.BB.CC", "azul")
-d.reportar()
+#d= Dispositivo("172.16.31.10", "AA.BB.CC", "azul")
+#d.reportar()
+
+
+
+class ImpresoraRed:
+    def __init__(self, ip, modelo):
+        self.ip = ip
+        self.modelo = modelo
+        self.paginas_impresas = 0
+
+    def imprimir(self, cantidad):
+        self.paginas_impresas += cantidad
+
+    def reportar(self):
+        print(f"IP: {self.ip}, Modelo: {self.modelo}, Paginas impresas: {self.paginas_impresas}")
+
+imp = ImpresoraRed("172.16.31.10", "HP_LaserJet")
+imp.imprimir(2)
+imp.imprimir(12)
+imp.reportar()
